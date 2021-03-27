cdddb41f1605cbfcf7ab680c54cba499a3056ba0
gudtjr3437 <gudtjr3437@naver.com>
2021-03-27 14:05:15 +0900

    #0.7 Classes on Typescript part One

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+//class = js 파일 상에서 나타남 (react,express,node 등등을 사용하게된다면 class 사용해야함) , interface = js 파일상에서 안나타남 (ts측면에서 더 안전함)
+class Human {
+    constructor(name, age, gender) {
+        //constructor=생성자  클래스가 시작할때마다 호출되는 method
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
 //interface만 JS에서 작동 안함
-const person = {
-    name: "HyeongSeok",
-    age: 25,
-    gender: "Male"
-};
+const Seok = new Human("Hyeong", 26, "Male");
 const sayHi = (person) => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
 //아무런 값도 반환하지 않을경우 : void | 문자열 반환시 string
-console.log(sayHi(person));
+console.log(sayHi(Seok));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
-interface Human {
-  name:string,
-  age:number,
-  gender:string;
+//class = js 파일 상에서 나타남 (react,express,node 등등을 사용하게된다면 class 사용해야함) , interface = js 파일상에서 안나타남 (ts측면에서 더 안전함)
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    //constructor=생성자  클래스가 시작할때마다 호출되는 method
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 //interface만 JS에서 작동 안함
 
-const person={
-  name:"HyeongSeok",
-  age:25,
-  gender:"Male"
-};
+const Seok = new Human("Hyeong", 26, "Male");
 
-
-const sayHi = (person:Human):string => {
+const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
 //아무런 값도 반환하지 않을경우 : void | 문자열 반환시 string
-console.log(sayHi(person));
+console.log(sayHi(Seok));
 export {};

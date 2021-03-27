da77b8c45ca0674d726383caeba2eb0a74518082
gudtjr3437 <gudtjr3437@naver.com>
2021-03-27 14:43:28 +0900

    #0.10 Creating a Block part Three

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const CryptoJS = require("crypto-js");
 class Block {
     constructor(index, hash, previouseHash, data, timestamp) {
         this.index = index;
@@ -9,7 +10,12 @@ class Block {
         this.timestamp = timestamp;
     }
 }
+Block.calculateBlockHash = (index, previouseHash, timestamp, data) => CryptoJS.SHA256(index + previouseHash + timestamp + data).toString();
 const genesisBlock = new Block(0, "202023232", "", "Hello", 1234556);
 let blockchain = [genesisBlock];
+const getBlockChain = () => blockchain;
+const getLatestBlock = () => blockchain[blockchain.length - 1];
+const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
 console.log(blockchain);
+//calculateBlockHash , getBlockChain , getLatestBlock ,getNewTimeStamp
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+ import * as CryptoJS from "crypto-js";
+ 
  class Block{
    public index: number;
    public hash :string;
    public previouseHash :string;
    public data :string;
    public timestamp :number;
+
+   static calculateBlockHash=(
+    index:number,
+    previouseHash:string,
+    timestamp:number,
+    data:string
+   ):string => CryptoJS.SHA256(index + previouseHash + timestamp + data).toString();
+
    constructor(index: number,
     hash :string,
      previouseHash :string,
@@ -19,7 +29,16 @@
 
  const genesisBlock:Block = new Block(0,"202023232","","Hello",1234556);
 
- let blockchain:[Block] = [genesisBlock];
+ let blockchain:Block[] = [genesisBlock];
+
+ const getBlockChain = (): Block[] =>blockchain;
+
+ const getLatestBlock = (): Block => blockchain[blockchain.length -1];
+
+ const getNewTimeStamp = ():number => Math.round(new Date().getTime()/1000);
 
  console.log(blockchain);
+
 export {};
+
+//calculateBlockHash , getBlockChain , getLatestBlock ,getNewTimeStamp

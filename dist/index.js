fbdb08116b98012cca66af2395c163740bb985ab
gudtjr3437 <gudtjr3437@naver.com>
2021-03-27 13:50:13 +0900

    #0.6 Classes on Typscript part One

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const sayHi = (name, age, gender) => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+//interface만 JS에서 작동 안함
+const person = {
+    name: "HyeongSeok",
+    age: 25,
+    gender: "Male"
+};
+const sayHi = (person) => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
 //아무런 값도 반환하지 않을경우 : void | 문자열 반환시 string
-console.log(sayHi("HyeongSeok", 25, "Male"));
+console.log(sayHi(person));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
-const sayHi = (name:string, age:number, gender:string):string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+interface Human {
+  name:string,
+  age:number,
+  gender:string;
+}
+//interface만 JS에서 작동 안함
+
+const person={
+  name:"HyeongSeok",
+  age:25,
+  gender:"Male"
+};
+
+
+const sayHi = (person:Human):string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
 //아무런 값도 반환하지 않을경우 : void | 문자열 반환시 string
-console.log(sayHi("HyeongSeok",25,"Male"));
+console.log(sayHi(person));
 export {};

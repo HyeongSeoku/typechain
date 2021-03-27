782e7b883a915a72b8966831928885d9248fa857
gudtjr3437 <gudtjr3437@naver.com>
2021-03-27 14:19:54 +0900

    #0.8 Blockchain Creating a Block

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-//class = js 파일 상에서 나타남 (react,express,node 등등을 사용하게된다면 class 사용해야함) , interface = js 파일상에서 안나타남 (ts측면에서 더 안전함)
-class Human {
-    constructor(name, age, gender) {
-        //constructor=생성자  클래스가 시작할때마다 호출되는 method
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    constructor(index, hash, previouseHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previouseHash = previouseHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
-//interface만 JS에서 작동 안함
-const Seok = new Human("Hyeong", 26, "Male");
-const sayHi = (person) => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-};
-//아무런 값도 반환하지 않을경우 : void | 문자열 반환시 string
-console.log(sayHi(Seok));
+const genesisBlock = new Block(0, "202023232", "", "Hello", 1234556);
+let blockchain = [genesisBlock];
+console.log(blockchain);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
-//class = js 파일 상에서 나타남 (react,express,node 등등을 사용하게된다면 class 사용해야함) , interface = js 파일상에서 안나타남 (ts측면에서 더 안전함)
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    //constructor=생성자  클래스가 시작할때마다 호출되는 method
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
-  }
-}
-//interface만 JS에서 작동 안함
+ class Block{
+   public index: number;
+   public hash :string;
+   public previouseHash :string;
+   public data :string;
+   public timestamp :number;
+   constructor(index: number,
+    hash :string,
+     previouseHash :string,
+    data :string,
+    timestamp :number){
+      this.index=index;
+      this.hash=hash;
+      this.previouseHash=previouseHash;
+      this.data=data;
+      this.timestamp=timestamp;
+   }
+ }
 
-const Seok = new Human("Hyeong", 26, "Male");
+ const genesisBlock:Block = new Block(0,"202023232","","Hello",1234556);
 
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-};
-//아무런 값도 반환하지 않을경우 : void | 문자열 반환시 string
-console.log(sayHi(Seok));
+ let blockchain:[Block] = [genesisBlock];
+
+ console.log(blockchain);
 export {};
